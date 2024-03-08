6c16ce161c1bf46657024e6a89823154e377e70c
Marclin Abarracoso <marclinabarracoso2@gmail.com>
2024-03-08 23:14:20 +0800

    Added tokenizeText function

--- pracset02/index.php
+++ pracset02/index.php
@@ -1,3 +1,23 @@
+<! -- Authors: Abarracoso, Marclin
+                Nepomuceno, Mark Dhenniel -->
+
+<?php
+
+function tokenizeText($text) {
+    // Tokenize the text into words, removing punctuation and converting to lowercase
+    return preg_split('/\W+/', strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $text = $_POST['text'];
+    $sortOrder = $_POST['sort'];
+    $limit = $_POST['limit'];
+
+    // Tokenize the input text
+    $words = tokenizeText($text);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +29,7 @@
 <body>
     <h1>Word Frequency Counter</h1>
     
-    <form action="process.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="text">Paste your text here:</label><br>
         <textarea id="text" name="text" rows="10" cols="50" required></textarea><br><br>
         
